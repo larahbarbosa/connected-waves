56a53acf35cc7e16c6a90d905ad138d65e4baa67
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-03-07 23:18:12 -0300

    jest.config

---
+++ test/jest.config.js
@@ -0,0 +1,12 @@
+const { resolve } = required('path');
+const root = resolve(__dirname, '..');
+const rootConfig = required(`${root}/jest.config.js`);
+
+module.exports = { ... rootConfig, ... {
+    rootDir: root,
+    displayName: "end2end-tests",
+    setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
+}
+}
+ 
