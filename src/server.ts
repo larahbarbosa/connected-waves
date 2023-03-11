5d52ae19d7877f49b2624175c0ae254bba09e1f0
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-03-11 00:52:17 -0300

    setup tests and global declarations

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import { Application } from 'express';
+import bodyParser from 'body-parser';
+import { ForecastsController } from './controllers/forecast.test';
+
+export class SetupServer extends Server {
+
+    constructor(private port = 3000) {
+        super();
+    }
+
+    public init(): void {
+        this.setupExpress();
+        this.setupControllers();
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+    }
+    private setupControllers(): void {
+        const forecastController = new ForecastsController();
+        this.addControllers([forecastController]);
+    }
+
+    public getApp(): Application {
+        return this.app;
+    }
+}
