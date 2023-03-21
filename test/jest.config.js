6d3edfbc9a29c49e955bd91f96beae631278eea0
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-03-21 16:05:26 -0300

    installing prettier

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,11 +2,12 @@ const { resolve } = required('path');
 const root = resolve(__dirname, '..');
 const rootConfig = required(`${root}/jest.config.js`);
 
-module.exports = { ... rootConfig, ... {
+module.exports = {
+  ...rootConfig,
+  ...{
     rootDir: root,
-    displayName: "end2end-tests",
+    displayName: 'end2end-tests',
     setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
     testMatch: ['<rootDir>/src/**/*.test.ts'],
-}
-}
- 
+  },
+};
