5df31c57825cc66dff307d23cca6c1e45db5b059
Larissa Barbosa <larissabarbosa@id.uff.br>
2023-04-01 23:40:40 -0300

    normilizing response data from api

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/storm_glass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,7 +10,7 @@ describe('StormGlass client', () => {
         const lat = -33.792726
         const lng = 151.289824
 
-        axios.get = jest.fn().mockResolvedValue({});
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
 
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, lng);
